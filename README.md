96496716770041d1cc678223c22facc67ed13093
Michael Schmitt <mschmitt@fastmail.fm>
2023-01-15 19:09:24 -0600

    Add downloader

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# twitter-like-exporter
-Hacky Python scripts for downloading your Twitter likes &amp; converting to HTML
+# Twitter Likes Exporter
+
+Hacky Python scripts for downloading your Twitter likes and converting to HTML. Includes support for downloading user avatars and image media in tweets. Uses the GraphQL API powering Twitter.com, the equivalent of you scrolling through all your likes in your web browser.
+
+Note only currently supports grabbing the liked tweet. So if it was a quote tweet, does not download the RT'd tweet. If it's a reply or part of a thread, does not download the other tweets.
+
+Meant to jumpstart you to at least getting your tweets offline from Twitter, if not building something much better!
+
+## Setup
+
+Intended for use with Python 3.7. First install requirements:
+
+```bash
+pip install -r requirements.txt
+```
+
+(It's just `requests`). Next you'll need to populate the `.env` file with credentials needed to match how your web browser gets your likes from Twitter.com:
+
+1. Open up your web browser and ensure the Network web debugging tab is open so you can inspect network requests
+2. Navigate to `https://twitter.com/<your_user_handle>/likes`
+3. Look for a network request to an `api.twitter.com` domain path ending in `/Likes`
+4. From the request headers:
+    a. Copy the `Authorization` value and save as `HEADER_AUTHORIZATION` in `.env`
+    b. Copy the `Cookies` value and save as `HEADER_COOKIES` in `.env`
+    c. Copy the `x-csrf-token` value and save as `HEADER_CSRF` in `.env`
+5. Find your Twitter user ID (available in the `/Likes` request params, or elsewhere) and save as `USER_ID` in `.env`
+
+
+### Download Likes to JSON
+
+By default, liked tweets will be saved to a file `liked_tweets.json` in this repo's folder path. If you'd like to override this, set new path as `OUTPUT_JSON_FILE_PATH` in `.env`.
+
+Run as follows:
+
+```bash
+python download_tweets.py
+```
+
+
+### Convert JSON Likes to HTML
+
+WIP
---
+++ download_tweets.py
@@ -0,0 +1,117 @@
+import configparser
+import json
+import requests
+
+class TweetDownloader():
+
+    def __init__(self):
+        # Load in user specific data from .env file
+        config = configparser.ConfigParser()
+        config.read('.env')
+        self.twitter_user_id = config.get('TWITTER', 'USER_ID')
+        self.header_authorization = config.get('TWITTER', 'HEADER_AUTHORIZATION')
+        self.header_cookie = config.get('TWITTER', 'HEADER_COOKIES')
+        self.header_csrf = config.get('TWITTER', 'HEADER_CSRF')
+        self.output_json_file_path = config.get('TWITTER', 'OUTPUT_JSON_FILE_PATH')
+
+    def retrieve_all_likes(self):
+        all_tweets = []
+
+        likes_page = self.retrieve_likes_page()
+        page_cursor = self.get_cursor(likes_page)
+        old_page_cursor = None
+        current_page = 1
+
+        while likes_page and page_cursor and page_cursor != old_page_cursor:
+            print(f"Fetching likes page: {current_page}...")
+            current_page += 1
+            for raw_tweet in likes_page:
+                output_tweet = parse_tweet(raw_tweet)
+                if output_tweet:
+                    all_tweets.append(output_tweet)
+            old_page_cursor = page_cursor
+            likes_page = get_likes(cursor=page_cursor)
+            page_cursor = get_cursor(likes_page)
+
+        with open(self.output_json_file_path, 'w') as f:
+            f.write(json.dumps(all_tweets))
+
+    def retrieve_likes_page(self, cursor=None):
+        likes_url = 'https://api.twitter.com/graphql/QK8AVO3RpcnbLPKXLAiVog/Likes'
+        variables_data_encoded = json.dumps(self.likes_request_variables_data(cursor=cursor))
+        features_data_encoded = json.dumps(self.likes_request_features_data())
+        response = requests.get(
+            likes_url,
+            params={"variables": variables_data_encoded, "features": features_data_encoded},
+            headers=headers
+        )
+        return self.extract_likes_entries(response.json())
+
+    def extract_likes_entries(self, raw_data):
+        return raw_data['data']['user']['result']['timeline_v2']['timeline']['instructions'][0]['entries']
+
+    def get_cursor(self, page_json):
+        return page_json[-1]['content']['value']
+
+    def likes_request_variables_data(self, cursor=None):
+        variables_data = {
+            "userId": self.twitter_user_id,
+            "count": 100,
+            "includePromotedContent": False,
+            "withSuperFollowsUserFields": False,
+            "withDownvotePerspective": False,
+            "withReactionsMetadata": False,
+            "withReactionsPerspective": False,
+            "withSuperFollowsTweetFields": False,
+            "withClientEventToken": False,
+            "withBirdwatchNotes": False,
+            "withVoice": False,
+            "withV2Timeline": True
+        }
+        if cursor:
+            variables_data["cursor"] = cursor
+        return variables_data
+
+    def likes_request_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Accept': '*/*',
+            'Authorization': self.header_authorization,
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Host': 'api.twitter.com',
+            'Origin': 'https://twitter.com',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+            'Referer': 'https://twitter.com/',
+            'Connection': 'keep-alive',
+            'Cookie': self.header_cookie,
+            'x-twitter-active-user': 'yes',
+            'x-twitter-client-language': 'en',
+            'x-csrf-token': self.header_csrf,
+            'x-twitter-auth-type': 'OAuth2Session'
+        }
+
+    def likes_request_features_data(self):
+        return {
+            "responsive_web_twitter_blue_verified_badge_is_enabled": True,
+            "verified_phone_label_enabled": False,
+            "responsive_web_graphql_timeline_navigation_enabled": True,
+            "view_counts_public_visibility_enabled": True,
+            "view_counts_everywhere_api_enabled": True,
+            "longform_notetweets_consumption_enabled": False,
+            "tweetypie_unmention_optimization_enabled": True,
+            "responsive_web_uc_gql_enabled": True,
+            "vibe_api_enabled": True,
+            "responsive_web_edit_tweet_api_enabled": True,
+            "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True,
+            "standardized_nudges_misinfo": True,
+            "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": False,
+            "interactive_text_enabled": True,
+            "responsive_web_text_conversations_enabled": False,
+            "responsive_web_enhance_cards_enabled": False
+        }
+
+if __name__ == '__main__':
+    downloader = TweetDownloader()
+    print(f'Starting retrieval of likes for Twitter user {downloader.twitter_user_id}...')
+    downloader.retrieve_all_likes()
+    print(f'Done. Likes JSON saved to: {downloader.output_json_file_path}')
